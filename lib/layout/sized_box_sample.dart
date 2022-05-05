5607449712ecdb2cc7efce19e53e084c08dcb825
Kyosuke Takayama <loiseau@gmail.com>
2022-05-05 20:02:54 +0900

    [style] remove commented code

--- lib/layout/sized_box_sample.dart
+++ lib/layout/sized_box_sample.dart
@@ -47,57 +47,5 @@ class SizedBoxSample extends StatelessWidget {
         ),
       ),
     );
-
-    // Row(
-    //   children: [
-    //     SizedBox(
-    //       width: 100,
-    //       child: Column(
-    //         children: [
-    //           Expanded(
-    //             child: Container(
-    //               color: Colors.green,
-    //             ),
-    //           ),
-    //           Container(
-    //             color: Colors.red,
-    //             height: 100,
-    //           ),
-    //           Expanded(
-    //             child: Container(
-    //               color: Colors.green,
-    //             ),
-    //           ),
-    //           Container(
-    //             color: Colors.red,
-    //             height: 100,
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //     SizedBox(
-    //       width: 100,
-    //       child: Column(
-    //         children: [
-    //           Container(
-    //             color: Colors.blue,
-    //             height: 100,
-    //           ),
-    //           Expanded(
-    //             child: Container(
-    //               color: Colors.yellow,
-    //             ),
-    //           ),
-    //           Container(
-    //             color: Colors.blue,
-    //             height: 100,
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //   ],
-    // ),
-    // ),
-    // );
   }
 }
